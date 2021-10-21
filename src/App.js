ded7704fc616c16e292832d121296c5c4d4fd0cc
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-10-21 15:55:11 -0500

    setting redireccion in login plus error msg

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Admi from './components/Admi';
 import WaiterFirstView from './components/WaiterFirstView';
 import PreviewCompletedOrders from './components/PreviewCompletedOrders';
 import DeliveredOrdersPreview from './components/DeliveredOrdersPreview';
-import axios from 'axios';
+// import axios from 'axios';
 
 
 function App() {
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,103 +1,125 @@
 import React, { useState } from "react";
+import { Redirect } from 'react-router-dom'
 import admi from '../img/admi.svg';
 import chef from '../img/chef.svg';
 import waiter from '../img/waiter.svg';
 
 const Login = () => {
-  const [datos, setDatos] = useState({
+  const [data, setData] = useState({
     email: "",
     password: "",
   });
 
+  const [option, setOption] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const [error, setError] = useState(false);
+
+
+
+  const handleSelectOption = (event) => {
+    console.log(event.target.value);
+    setOption(event.target.value);
+  }
 
   const handInputChange = (e) => {
     //console.log(e.target.value);
-    setDatos({
-      ...datos,
+    setData({
+      ...data,
       [e.target.name]: e.target.value
     })
   };
 
-  const enviarDatos = (e) => {
+  const sendForm = (e) => {
     e.preventDefault();
-    //verificacion del ruteo según sus opciones 
-
-    console.log(datos.email + datos.password);
-    window.open("/waiter", "_self");
+    if (data.email && data.password && option) {
+      setRedirect(true);
+    } else {
+      setError(true)
+    }
   }
 
-
-
   return (
-    <div className="App bg-scroll bg-cover h-screen flex px-4 sm:px-0 mx-auto">
+    redirect ? (
+      <Redirect to={`/${option}`} />
+    ) : (
+      <div className="App bg-scroll bg-cover h-screen flex px-4 sm:px-0 mx-auto" >
 
-      <section className="sm:mx-auto my-auto bg-gray-900 bg-opacity-60 shadow-xl rounded-3xl py-4">
+        <section className="sm:mx-auto my-auto bg-gray-900 bg-opacity-60 shadow-xl rounded-3xl py-4">
 
-        <div className="cangreburgertitle">
-          <h1>Welcome to
-            Cangre Burger</h1>
-        </div>
+          <div className="cangreburgertitle">
+            <h1>Welcome to
+              Cangre Burger</h1>
+          </div>
 
-        <div className="login__options">
-          <h2 className='enter_as '> ENTER AS:</h2>
-          <div className='characters--wrapper'>
+          <div className="login__options">
+            <h2 className='enter_as '> ENTER AS:</h2>
+            <div className='characters--wrapper'>
 
-            <div className='flex flex-col'>
-              <img className="characters--image" src={admi} alt="logo" />
-              <div className='flex flex-row items-center gap-2 pl-4'>
-                <input type='radio'></input>
-                <p className="text--options"> ADMI</p>
-              </div>
-            </div>
-
-            <div className='flex flex-col' >
-              <img className='characters--image' src={waiter} alt="logo" />
-              <div className='flex flex-row items-center gap-2 pl-2'>
-                <input type='radio'></input>
-                <p className='text--options'> WAITER</p>
-              </div>
-            </div>
-
-            <div className='flex flex-col'>
-              <img className="characters--image" src={chef} alt="logo" />
-              <div className='flex flex-row items-center gap-2 px-4'>
-                <input type='radio'></input>
-                <p className="text--options"> CHEF</p>
+              <div className='flex flex-col'>
+                <img className="characters--image" src={admi} alt="logo" />
+                <div className='flex flex-row items-center gap-2 pl-4'>
+                  <input type='radio' name={'role'} value={'admi'} onClick={handleSelectOption} />
+                  <p className="text--options"> ADMI</p>
+                </div>
               </div>
 
+              <div className='flex flex-col' >
+                <img className='characters--image' src={waiter} alt="logo" />
+                <div className='flex flex-row items-center gap-2 pl-'>
+                  <input type='radio' name={'role'} value={'waiter'} onClick={handleSelectOption} />
+                  <p className='text--options'> WAITER</p>
+                </div>
+              </div>
+
+              <div className='flex flex-col'>
+                <img className="characters--image" src={chef} alt="logo" />
+                <div className='flex flex-row items-center gap-2 px-4'>
+                  <input type='radio' name={'role'} value={'chef'} onClick={handleSelectOption} />
+                  <p className="text--options"> CHEF</p>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+
+          <form onSubmit={sendForm} className="text-center">
+            <div>
+              <input className='input input--email'
+                type={"text"}
+                placeholder={"Email"}
+                name={"email"}
+                onChange={handInputChange}
+                required
+              />
             </div>
 
-          </div>
-        </div>
+            <div>
+              <input className='input input--password'
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={handInputChange}
+                required
+              />
+            </div>
+            {error ? (
+              <div>
+                <p>Please, complete all fields</p>
+              </div>
+            ) : null}
 
-        <form onSubmit={enviarDatos} className="text-center">
-          <div>
-            <input className='input input--email'
-              type={"text"}
-              placeholder={"Email"}
-              name={"email"}
-              onChange={handInputChange}
-              required
-            />
-          </div>
 
-          <div>
-            <input className='input input--password'
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handInputChange}
-              required
-            />
-          </div>
+            <div>
+              <button className='input button--login' type={'submit'}> LOG IN </button>
+            </div>
+          </form>
+        </section>
 
-          <div>
-            <button className='input button--login' type="submit"> LOG IN </button>
-          </div>
-        </form>
-      </section>
+      </div >)
 
-    </div >
-  );
+  )
+
 };
+
 export default Login;
