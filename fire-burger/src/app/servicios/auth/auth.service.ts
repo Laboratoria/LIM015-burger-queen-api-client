4140d773faa0be89286921d934d01c67c313843c
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-10-24 11:58:45 -0500

    wip: add funcionality views usuarios and productos

--- fire-burger/src/app/servicios/auth/auth.service.ts
+++ fire-burger/src/app/servicios/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -18,5 +19,29 @@ export class AuthService {
     this.objectAuth['email']= user.email;
     this.objectAuth['password']= user.password;
     return this.http.post(this.Url, this.objectAuth);
+  }
+  postUser(data : any){
+    return this.http.post<any>('https://fireburguer.herokuapp.com/users', data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  } 
+  getUser(){
+    return this.http.get<any>('https://fireburguer.herokuapp.com/users')
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  } 
+  updateUser(data : any, id : number){
+    return this.http.put<any>('https://fireburguer.herokuapp.com/users/'+id, data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  } 
+  deleteUser(id : number){
+    return this.http.delete<any>('https://fireburguer.herokuapp.com/users/'+ id)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
   } 
 }
--- fire-burger/src/app/vistas/usuarios/usuarios.component.ts
+++ fire-burger/src/app/vistas/usuarios/usuarios.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/servicios/auth/auth.service';
+import { UserModel } from './usuarios.model';
+
 
 @Component({
   selector: 'app-usuarios',
@@ -9,14 +12,35 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class UsuariosComponent implements OnInit {
    
   formValue !: FormGroup;
-  constructor(private formbuilder: FormBuilder) { }
+  userModelObject : UserModel = new UserModel();
+  constructor(private formbuilder: FormBuilder,
+    private auth : AuthService) { }
 
   ngOnInit(): void {
     this.formValue = this.formbuilder.group({
       usuario : [''],
-      cargo : [''],
       contraseña: [''],
+      cargo : [''],
+      admin : [''],
     })
   }
+  postUserDetails(){
+    this.userModelObject.usuario = this.formValue.value.usuario;
+    this.userModelObject.contraseña = this.formValue.value.contraseña;
+    this.userModelObject.cargo = this.formValue.value.cargo;
+    this.userModelObject.admin = this.formValue.value.admin;
 
+    this.auth.postUser(this.userModelObject)
+    .subscribe(res=>{
+      console.log(res);
+      alert("¡Usuario agregado!")
+      let ref = document.getElementById('cancel')
+      ref?.click();
+      this.formValue.reset();
+    },
+      err=>{
+        alert("Ups, ocurrió un error");
+      }
+    )
+  }
 }
