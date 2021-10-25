0f3704597715a61e3f4b1f75dc42c774c2d41dbf
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-10-24 19:54:15 -0500

    wip: login/cerrar sesión(#8)

--- fire-burger/src/app/vistas/login/login.component.ts
+++ fire-burger/src/app/vistas/login/login.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl, Validators } from '@angular/forms'
-import { AuthService } from '../../servicios/auth/auth.service'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../servicios/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-    loginForm = new FormGroup({
-    email: new FormControl('',Validators.required),
-    password : new FormControl('',Validators.required)
-  })
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
-
-  constructor(private AuthService: AuthService) { }
+  constructor(private AuthService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.useLocalStorage();
   }
-  login(){
-    this.AuthService.Auth(this.loginForm.value).subscribe(obs=> console.log(obs))
-    //console.log(this.loginFormYup.value);
+  useLocalStorage() {
+    if (localStorage.getItem('token')) {
+      this.router.navigate(['/menu']);
+    }
+  }
+  login() {
+    this.AuthService.Auth(this.loginForm.value).subscribe(
+      (data) => {
+        localStorage.setItem('token', data.token);
+        console.log('success', data);
+        this.router.navigate(['/menu']);
+        //console.log(this.loginFormYup.value);
+      },
+      (error) => {
+        if (error.status > 400) {
+          alert('Datos incorrectos');
+        } else if (error.status == 400) {
+          alert('Datos incompletos');
+        }
+      }
+    );
   }
 }
